10d9032b53c6579e7d818360da4bd89a34a52c0d
lovekesh_jindal <lovekeshjindal05@gmail.com>
2026-07-12 14:05:56 +0530

    Modify getUnmarkedRecords to throw ZinggClientException
    Updated getUnmarkedRecords method to declare throws ZinggClientException for better error handling.

--- common/client/src/main/java/zingg/common/client/IZinggModelInfo.java
+++ common/client/src/main/java/zingg/common/client/IZinggModelInfo.java
@@ -3,8 +3,8 @@ package zingg.common.client;
 //TODO need to revisit this interface
 public interface IZinggModelInfo<S,D,R,C> {
     ZFrame<D,R,C>  getMarkedRecords();
-
-    ZFrame<D,R,C>  getUnmarkedRecords();
+   // Declares throws ZinggClientException so that implementation can propagate a genuine read failure to the caller instead of swallowing it and returning null
+    ZFrame<D,R,C>  getUnmarkedRecords() throws ZinggClientException;
 
     Long getMarkedRecordsStat(ZFrame<D, R, C> markedRecords, long value);
 
