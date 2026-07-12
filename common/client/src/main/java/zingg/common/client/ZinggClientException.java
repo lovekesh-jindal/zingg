d6da96b67826aadd0dc23830cf266e8412aac695
lovekesh_jindal <lovekeshjindal05@gmail.com>
2026-07-12 13:45:54 +0530

    Update ZinggClientException.java

--- common/client/src/main/java/zingg/common/client/ZinggClientException.java
+++ common/client/src/main/java/zingg/common/client/ZinggClientException.java
@@ -6,8 +6,11 @@ package zingg.common.client;
  * @author sgoyal
  *
  */
-
-public class ZinggClientException extends Throwable {
+// DESIGN ISSUE : extends Exception instead of throwable
+// REASON : as a direct throwable subclass it was a sibling of exception so catch(exception e) couldn't catch it (it behaved like an Error)
+// but if an Error ( like out of memory , stack overflow ) came it will still flow and  ZinggClientException not able to catch that 
+// now as a exception subclass it is now caught by an ordinary exception , while Error still propagate
+public class ZinggClientException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
